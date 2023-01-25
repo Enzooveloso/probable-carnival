d8432a1e99aba9bce8bcf615d608d70cc914094d
Enzo Veloso <enzoveloso.2002@gmail.com>
2023-01-24 21:28:44 -0300

    continuacao

---
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+
+namespace Tarefas.Web.Models
+{
+    public class TarefaViewModel
+    {
+        [DisplayName("Titulo")]
+        public string Titulo { get;set;}
+        [DisplayName("Descricao")]
+        public string Descricao {get;set;}
+    }
+}
