343d5381c786d0e573343027c57c9f90791fb6fd
Enzo Veloso <enzoveloso.2002@gmail.com>
2023-01-25 21:36:48 -0300

    dia02

--- Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -1,13 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers
-{
-        public class TarefaController : Controller
+{      
+    public class TarefaController : Controller
+    {
+        public List<TarefaViewModel> listaDeTarefas{get;set; }
+        public TarefaController()
         {
-            public IActionResult Create()
+            listaDeTarefas = new List<TarefaViewModel>()
             {
-                return View();
-            }
+                 new TarefaViewModel(){ Titulo = "Indo para academia", Descricao = "Segunda a sabado"},
+                 new TarefaViewModel(){ Titulo = "Fazendo almoco", Descricao = "All days"}
+                    
+             };
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public  IActionResult Index()
+        {
+             
+                    return View(listaDeTarefas);
+         }
+        public IActionResult Details(int id)
+        {
+            var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+            return View(tarefa);
+        }
+    }   
 }
 
--- Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -4,8 +4,13 @@ namespace Tarefas.Web.Models
 {
     public class TarefaViewModel
     {
+
+        [DisplayName("codigo")]
+        public int Id {get;set;}
+
         [DisplayName("Titulo")]
         public string Titulo { get;set;}
+
         [DisplayName("Descricao")]
         public string Descricao {get;set;}
     }
